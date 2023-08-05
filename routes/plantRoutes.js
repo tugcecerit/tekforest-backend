f72bd38db0431b518424b4d7173d4fa011ef0f2e
tugce <tugcetopkaccerit@gmail.com>
2023-08-05 17:52:26 -0400

    trying to fix routes

--- routes/plantRoutes.js
+++ routes/plantRoutes.js
@@ -3,7 +3,7 @@ const { plantCtrl } = require('../controllers')
 
 router.get('/', plantCtrl.getPlant)
 router.post('/', plantCtrl.createPlant)
-router.put('/:id', plantCtrl.updatePlant)
+router.put('/:id/edit', plantCtrl.updatePlant)
 router.delete('/:id', plantCtrl.deletePlant)
 
 module.exports = router;
